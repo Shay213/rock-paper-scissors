2dfa91475561ae0adf909dbe052bdf51835a7479
Shay213 <123243481+Shay213@users.noreply.github.com>
2023-01-27 20:19:50 +0100

    Fix playerChoice validation and create round logic
    Function getPlayerChoice used prompt to get user choice and if choice
    matched any value in VALID_CHOICES it returned player choice or alert
    message. VALID_CHOICES values are in lower case so player choice was
    converted to lower case.
    
    The problem is that player choice is not always a string.
    Also after adding a function that returns round result there was a
    problem where this function was returning value even tho player choice
    was not valid value.
    
    Now function isPlayerChoiceValid checks if type of player choice is
    string so there is no more problem with player choice to lower case.
    This function also solves second problem because now we run
    getRoundResult only if player choice is a valid value.

--- javascript.js
+++ javascript.js
@@ -1,13 +1,38 @@
 const getRandomInt = (max) => Math.floor(Math.random()*max);
 const VALID_CHOICES = ["rock", "paper", "scissors"];
 
-function getPlayerChoice(){
-    let choice = prompt("Please enter your choice(rock,paper,scissors): ");
-    let isChoiceValid = VALID_CHOICES.includes(choice.toLowerCase());
+function completeGameRound(){
+    let playerChoice = prompt("Please enter your choice(rock,paper,scissors): ");
+    let pcChoice = VALID_CHOICES[getRandomInt(3)];
 
-    return isChoiceValid ? choice : alert("Invalid choice!");
+    return isPlayerChoiceValid() ? getRoundResult() : null;
+
+    function isPlayerChoiceValid(){
+        let playerChoiceIsString = typeof playerChoice === "string";
+        let playerChoiceInValidPool = VALID_CHOICES.includes(playerChoice.toLowerCase());
+
+        return playerChoiceIsString && playerChoiceInValidPool;
+    }
+    
+    function getRoundResult(){
+        // is it a draw?
+        if(playerChoice === pcChoice) return "draw";
+
+        let whoWon = "";
+        
+        switch(playerChoice){
+            case "rock":
+                pcChoice === "scissors" ? whoWon = "player":whoWon = "pc";
+                break;
+            case "paper":
+                pcChoice === "rock" ? whoWon = "player":whoWon = "pc";
+                break;
+            case "scissors":
+                pcChoice === "paper" ? whoWon = "player":whoWon = "pc";
+                break;
+        }
+        return whoWon;
+    }
 }
 
-function getComputerChoice(){
-    return VALID_CHOICES[getRandomInt(3)];
-}
+console.log(completeGameRound());
