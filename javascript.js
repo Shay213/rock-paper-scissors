088453288dc51d75eacdbad25eecd0878ba9458e
Shay213 <123243481+Shay213@users.noreply.github.com>
2023-01-28 15:20:23 +0100

    Fix user input validation and new functionality
    Changes in completeGameRound function:
    
    isPlayerChoiceValid function was checking type of user input and if
    input was in valid pool and it decided if playerChoice was a valid value
    toLowerCase conversion on playChoice was failing because playerChoice
    could have different type than string. Now this function checks type
    before allowing toLowerCase conversion and returns false if its
    different type than string.
    
    added if that checks if cancel button was pressed which allows us to
    exit program
    
    playerChoice and pcChoice variables are moved to startGame function so
    we can access them and display them to user.
    
    Added functionality:
    
    player chooses how many rounds he wants to play
    validation of how many rounds choice
    
    startGame is a main function, which main task is to get result of the
    round and act accordingly to received value

--- javascript.js
+++ javascript.js
@@ -1,38 +1,107 @@
-const getRandomInt = (max) => Math.floor(Math.random()*max);
+let getRandomInt = (max) => Math.floor(Math.random()*max);
 const VALID_CHOICES = ["rock", "paper", "scissors"];
 
-function completeGameRound(){
-    let playerChoice = prompt("Please enter your choice(rock,paper,scissors): ");
-    let pcChoice = VALID_CHOICES[getRandomInt(3)];
+function completeGameRound(playerChoice, pcChoice){
+    // if cancel button was pressed
+    if(playerChoice === null) return null;
 
-    return isPlayerChoiceValid() ? getRoundResult() : null;
+    return isPlayerChoiceValid() ? getRoundResult() : false;
 
     function isPlayerChoiceValid(){
         let playerChoiceIsString = typeof playerChoice === "string";
-        let playerChoiceInValidPool = VALID_CHOICES.includes(playerChoice.toLowerCase());
 
-        return playerChoiceIsString && playerChoiceInValidPool;
+        return playerChoiceIsString ? (() => {
+            let playerChoiceInValidPool = VALID_CHOICES.includes(playerChoice.toLowerCase());
+            return playerChoiceInValidPool;
+        })() : false;
     }
     
     function getRoundResult(){
-        // is it a draw?
-        if(playerChoice === pcChoice) return "draw";
+        if(playerChoice === pcChoice) return "It's a Tie!";
 
-        let whoWon = "";
+        let result = "";
         
         switch(playerChoice){
             case "rock":
-                pcChoice === "scissors" ? whoWon = "player":whoWon = "pc";
+                pcChoice === "scissors" ? result = "You Win! rock beats scissors":result = "You Lose! paper beats rock";
                 break;
             case "paper":
-                pcChoice === "rock" ? whoWon = "player":whoWon = "pc";
+                pcChoice === "rock" ? result = "You Win! paper beats rock":result = "You Lose! scissors beats paper";
                 break;
             case "scissors":
-                pcChoice === "paper" ? whoWon = "player":whoWon = "pc";
+                pcChoice === "paper" ? result = "You Win! scissors beats paper":result = "You Lose! rock beats scissors";
                 break;
         }
-        return whoWon;
+        return result;
     }
 }
 
-console.log(completeGameRound());
+function game(){
+    let playerRoundsWon = 0;
+    let pcRoundsWon = 0;
+    let roundResult = "";
+    let howManyRounds = Number(prompt("How many rounds would you like to play? "));
+    
+    if(howManyRounds === 0) 
+        return null;
+    else if(Number.isNaN(howManyRounds)){
+        alert("That's not a number!");
+        game();
+    }
+    else
+        howManyRounds = Math.round(howManyRounds);
+        startGame();
+
+    function startGame(){
+        for(let i=1; i <= howManyRounds; i++){
+            let playerChoice = prompt("Please enter your choice(rock,paper,scissors): ");
+            let pcChoice = VALID_CHOICES[getRandomInt(3)];
+
+            roundResult = completeGameRound(playerChoice, pcChoice);
+            
+            if(roundResult === null){
+                break;
+            } else if(!roundResult) {
+                i--;
+                alert("Your choice is not valid!");
+            } else{
+                updateScore();
+                displayRoundInfo(i, playerChoice, pcChoice);
+            }
+        }
+        if(playerRoundsWon > 0 || pcRoundsWon > 0)
+            finalMessage();
+    }
+
+    function updateScore(){
+        if(roundResult.includes("Win")){
+            playerRoundsWon++;
+        } else if(roundResult.includes("Lose")){
+            pcRoundsWon++;
+        } else{
+            playerRoundsWon++;
+            pcRoundsWon++;
+        }
+    }
+
+    function displayRoundInfo(currentRound, playerChoice, pcChoice){
+        alert(`Round ${currentRound}:
+        Your choice is: ${playerChoice}
+        PC choice is: ${pcChoice}
+        ${roundResult} 
+        You won ${playerRoundsWon === 1 ? `1 round!` : `${playerRoundsWon}rounds`}
+        PC won ${pcRoundsWon === 1 ? `1 round!` : `${pcRoundsWon}rounds`}
+        `);
+    }
+
+    function finalMessage(){
+        if(playerRoundsWon > pcRoundsWon)
+            alert("Congratulations, You won the game!");
+        else if(playerRoundsWon < pcRoundsWon)
+            alert("You Lost, that was a good game!");
+        else
+            alert("It's a Tie, that was a good game!");
+    }
+}
+
+game();
